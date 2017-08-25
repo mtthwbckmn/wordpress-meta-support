d15b9cb7ce73f2f15586d88a645eb7810ed64f3a
Matthew Beckman <mtthwbckmn@gmail.com>
2017-08-25 00:56:41 -0700

    V1.1 Changes
    Changed how the default site name was generated. Changed default link to work on non-singular pages. Added support for Facebook App ID.

--- README.md
+++ README.md
@@ -7,21 +7,18 @@ Copy and paste the code into your themes `functions.php` file, and edit the exam
 
 ## Set Default Variables
 * `defTitle` - **Title** - The title of your site as it should appear within the graph.
-* `defLink` - **Site Link** - The canonical URL of your site that will be used in the graph.
 * `defDesc` - **Description** - A one to two sentence description of your site.
 * `defImg` - **Image** - An image URL which should represent your site. Must begin with `http://` or `https://`.
 * `defWidth` - **Image Width** - In `pixels`, the width of the default image defined above. Only enter numbers.
 * `defHeight` - **Image Height** - In `pixels`, the height of the default image defined above. Only enter numbers.
-* `siteName` - **Site Name** - The name which should be displayed for the overall site.
 * `twitter` - **Twitter Username** - The Twitter username associated with the site. Do not include the `@` prefix.
->##### Note: The only time default values are used is when we're not able to automatically generate the necessary tags.
+>##### Note: The only time default values are used is when the code is not able to automatically generate the necessary tags. The default `defLink` and `siteName` variables were removed in the version 1.1 release.
 
 ----
 
 ### Upcoming Features
-* Add optional `fb_id` to the [Open Graph](http://ogp.me) code.
-* Pull in **link** from non-singular pages (archives, categories, etc), instead of falling back on default value.
 * Pull in **title** from `<title>` for non-singular pages (archives, categories, etc), instead of falling back on default value.
+* Set `type` to *website* on pages that aren't also posts.
 * Create version without comments, and minimized version.
 * Add option to use as a WordPress plugin.
 >##### Development Note: Features are added in order of priority. The top of the list is more highly prioritized than the bottom. Please submit feature requests & bug reports using GitHub [here](https://github.com/mtthwbckmn/wordpress-meta-support/issues).
@@ -30,4 +27,5 @@ Copy and paste the code into your themes `functions.php` file, and edit the exam
 * **[Matthew Beckman](https://matthewbeckman.co)** - [mtthwbckmn](https://twitter.com/mtthwbckmn)
 
 ### Change Log
-* **1.0** - **August 23, 2017** - First release
+* **1.1** - **August 25, 2017** - Removed the user variable `defLink`, and replaced with an automatically generated URL for non-singular pages. Removed the user variable `siteName`, and replaced with the site name defined in WordPress' settings. Added optional `fb:app_id` property for Facebook. Changed default `type` to website when on non-singular pages. Still defaults to article when on singular pages.
+* **1.0** - **August 23, 2017** - First release.
--- generator.php
+++ generator.php
@@ -3,27 +3,34 @@
 /* Add Open Graph & Twitter Card Support */
 function insertMeta() {
     
+    global $wp; // Get WordPress data.
     global $post; // Get post data.
-    
+     
     /* User Variables */
     $defTitle  = "My Site Title"; // Default Title
-    $defLink   = "https://example.com"; // Default Link
     $defDesc   = "My site's description."; // Default Description
     $defImg    = "https://example.com/image.jpg"; // Default Image
     $defWidth  = "500"; // Default Image Width
     $defHeight = "500"; // Default Image Height
-    $siteName  = "My Site"; // Site Name
+    $fbAppId   = "123myfacebookappid"; // Facebook App ID
     $twitter   = "my_twitter_account"; // Twitter Account
     
+    /* WordPress Variables */
+    $siteName = get_bloginfo( 'name' ); // Get WordPress site title.
+    $genLink  = home_url( add_query_arg( array(), $wp->request ) ); // Generate URL based on WordPress request.
+    
     if ( !is_singular() ) {
-        echo '<meta property="og:url" content="' . $defLink . '">';
-        echo '<meta property="og:type" content="article">';
+        echo '<meta property="og:url" content="' . $genLink . '">';
+        echo '<meta property="og:type" content="website">';
         echo '<meta property="og:description" content="' . $defDesc . '">';
         echo '<meta property="og:title" content="' . $defTitle . '">';
         echo '<meta property="og:site_name" content="' . $siteName . '">';
         echo '<meta property="og:image" content="' . $defImg . '">';
         echo '<meta property="og:image:height" content="' . $defHeight . '">';
         echo '<meta property="og:image:width" content="' . $defWidth . '">';
+        if ( $fbAppId ) {
+            echo '<meta property="fb:app_id" content="' . $fbAppId . '">';
+        }
         echo '<meta name="twitter:card" content="summary">';
         echo '<meta name="twitter:site" content="@' . $twitter . '">';
         echo '<meta name="twitter:title" content="' . $defTitle . '">';
@@ -43,21 +50,21 @@ function insertMeta() {
         if ( $link ) {
             echo '<meta property="og:url" content="' . $link . '">';
         } else {
-            echo '<meta property="og:url" content="' . $defLink . '">';
+            echo '<meta property="og:url" content="' . $genLink . '">';
         }
-            echo '<meta property="og:type" content="article">';
-        if ($desc) {
+        echo '<meta property="og:type" content="article">';
+        if ( $desc ) {
             echo '<meta property="og:description" content="' . $desc . '">';
         } else {
             echo '<meta property="og:description" content="' . $defDesc . '">';
         }
-        if ($title) {
+        if ( $title ) {
             echo '<meta property="og:title" content="' . $title . '">';
         } else {
             echo '<meta property="og:title" content="' . $defTitle . '">';
         }
-            echo '<meta property="og:site_name" content="' . $siteName . '">';
-        if ($img) {
+        echo '<meta property="og:site_name" content="' . $siteName . '">';
+        if ( $img ) {
             echo '<meta property="og:image" content="' . $img . '">';
             echo '<meta property="og:image:height" content="' . $width . '">';
             echo '<meta property="og:image:width" content="' . $height . '">';
@@ -66,25 +73,30 @@ function insertMeta() {
             echo '<meta property="og:image:height" content="' . $defWidth . '">';
             echo '<meta property="og:image:width" content="' . $defHeight . '">';
         }
-
+        
+        /* Facebook Options */
+        if ( $fbAppId ) {
+            echo '<meta property="fb:app_id" content="' . $fbAppId . '">';
+        }
+        
         /* Twitter Cards */
         echo '<meta name="twitter:card" content="summary">';
-        if ($twitter) {
+        if ( $twitter ) {
             echo '<meta name="twitter:site" content="@' . $twitter . '">';
         } else {
             // The Twitter username was left blank. Do nothing.
         }
-        if ($title) {
+        if ( $title ) {
             echo '<meta name="twitter:title" content="' . $title . '">';
         } else {
-        echo '<meta name="twitter:title" content="' . $defTitle . '">'; 
+            echo '<meta name="twitter:title" content="' . $defTitle . '">'; 
         }
-        if ($desc) {
+        if ( $desc ) {
             echo '<meta name="twitter:description" content="' . $desc . '">';
         } else {
             echo '<meta name="twitter:description" content="' . $defDesc . '">';
         }
-        if ($img) {
+        if ( $img ) {
             echo '<meta name="twitter:image" content="' . $img . '">';
         } else {
             echo '<meta name="twitter:image" content="' . $defImg . '">';
